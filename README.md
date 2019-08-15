e87c2ddac7cd5f88e80db0fc14c1a98f45ae7982
joehal1998@gmail.com <joehal1998@gmail.com>
2019-08-14 23:33:27 -0400

    node-pty installed

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# Cell
+This is a minimal example of getting a terminal running in Electron using [node-pty](https://github.com/Tyriar/node-pty) and [xterm.js](https://github.com/sourcelair/xterm.js).
 
+![](./images/preview.png)
 
-## License
+## Usage
 
-[CC0 1.0 (Public Domain)](LICENSE.md)
+```bash
+# Install npm dependencies using Electron's version of V8
+./npm_install.sh
+# Launch the app
+npm start
+```
--- renderer.js
+++ renderer.js
@@ -1,3 +1,25 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+var os = require('os');
+var pty = require('node-pty');
+var Terminal = require('xterm').Terminal;
+
+// Initialize node-pty with an appropriate shell
+const shell = process.env[os.platform() === 'win32' ? 'COMSPEC' : 'SHELL'];
+const ptyProcess = pty.spawn(shell, [], {
+  name: 'xterm-color',
+  cols: 80,
+  rows: 30,
+  cwd: process.cwd(),
+  env: process.env
+});
+
+// Initialize xterm.js and attach it to the DOM
+const xterm = new Terminal();
+xterm.open(document.getElementById('xterm'));
+
+// Setup communication between xterm.js and node-pty
+xterm.on('data', (data) => {
+  ptyProcess.write(data);
+});
+ptyProcess.on('data', function (data) {
+  xterm.write(data);
+});
